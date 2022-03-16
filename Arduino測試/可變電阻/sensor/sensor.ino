91891d449d934b4d52e4c00920144330a8cc9119
armyant0920 <armyant0920@gmail.com>
2022-03-16 23:44:59 +0800

    update sensor

--- Arduino測試/可變電阻/sensor/sensor.ino
+++ Arduino測試/可變電阻/sensor/sensor.ino
@@ -5,18 +5,15 @@ int sensor=A0;
 
 void setup() {
   Serial.begin(9600);
-   //pinMode(3,INPUT);
+   pinMode(3,INPUT);
 }
 
 void loop() {
   Read=analogRead(sensor);
-  light=(Read,0,1023,0,255);
-  Serial.println(light);
- // analogWrite(3,light);//3號接腳收類比訊號
-  delay(500);
-  
-  
-  
-  
+  Serial.println(Read);
+  light=map(Read,0,1023,0,255);//使用map將數字等比例轉換到0~255
+
+  analogWrite(3,light);//3號接腳收類比訊號
+  delay(200);
 
 }
