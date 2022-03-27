9973d26be29364f2b10944bc042b602d135af2b2
armyant0920 <armyant0920@gmail.com>
2022-03-27 10:25:19 +0800

    20220327 update

--- Arduino測試/蜂鳴器/tone/tone.ino
+++ Arduino測試/蜂鳴器/tone/tone.ino
@@ -3,7 +3,7 @@ int melody[]={NOTE_C4,NOTE_G4,NOTE_A3,NOTE_G3,NOTE_B3,NOTE_C4};
 int song[]={NOTE_G4,NOTE_E4,NOTE_E4,NOTE_F4,NOTE_D4,NOTE_D4,NOTE_C4,NOTE_D4};
 int buttonPin=12;
 int noteDur[]={4,8,8,4,4,4,4,4};
-int noteDur2[]={2,4,4,2,4,4,2,2};
+int noteDur2[]={4,4,4,4,4,4,4,4};
 
 void setup() {
 
@@ -15,9 +15,20 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   int state=digitalRead(buttonPin);
+
+
+      for(int now=0;now<8;now++){
+          int dur = 1000/noteDur2[now];
+          tone(8,song[now],dur);//8號腳位
+          int pause=dur*1.30;
+          delay(pause);
+
+          noTone(8);//8號腳位
+        
+        }
  
 
-  if(state==1){
+  /*if(state==1){//加上開關判斷
       Serial.println(state);
       digitalWrite(7, HIGH); 
       for(int now=0;now<8;now++){
@@ -31,7 +42,7 @@ void loop() {
         }
          digitalWrite(7, LOW); 
     
-    }
+    }*/
 
   
 
